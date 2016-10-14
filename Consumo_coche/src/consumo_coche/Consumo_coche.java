b8e140609c090cfaa60a3776a82f1173a30d7c0e
jquesadaabeijon <jquesadaabeijon@atanasoff32.danielcastelao.org>
2016-10-14 11:56:43 +0200

    Commit 3

--- Consumo_coche/src/consumo_coche/Consumo_coche.java
+++ Consumo_coche/src/consumo_coche/Consumo_coche.java
@@ -5,14 +5,14 @@ public class Consumo_coche {
 
     public static void main(String[] args) {
         //instanciar objeto
-        Consumo coche1 = new Consumo();
-        coche1.setLitros(50f);
-        coche1.setPrecioGasolina(1.57f);
+        Consumo consumo1 = new Consumo();
+        consumo1.setLitros(50f);
+        consumo1.setPrecioGasolina(1.57f);
         
-        Consumo coche2 = new Consumo(100f,50f,80f,1.57f);
-        System.out.println("Consumo medio = "+coche2.consumoMedio());
-        coche2.setLitros(75f);
-        coche2.mostrar();
+        Consumo consumo2 = new Consumo(100f,50f,80f,1.57f);
+        System.out.println("Consumo medio = "+consumo2.consumoMedio());
+        consumo2.setLitros(75f);
+        consumo2.mostrar();
     }
     
 }
