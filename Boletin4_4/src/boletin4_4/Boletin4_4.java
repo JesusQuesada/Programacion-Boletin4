42c87c5cd0722b4147829d31aff6b7fff191a9fb
Jesus Quesada <jmqa82@gmail.com>
2016-10-19 08:59:26 +0200

    Update Boletin4_4.java

--- Boletin4_4/src/boletin4_4/Boletin4_4.java
+++ Boletin4_4/src/boletin4_4/Boletin4_4.java
@@ -4,13 +4,13 @@ package boletin4_4;
 public class Boletin4_4 {
 
     public static void main(String[] args) {
-        Restaurante rest1 = new Restaurante();
-        rest1.añadirPulpo(78);
-        rest1.mostrarPulpo();
-        rest1.añadirPatatas(16);
-        rest1.mostrarPatatas();
-        rest1.calcularPersonas();
-        System.out.println("Cantidad personas = "+rest1.calcularPersonas());
+        Restaurante rest1 = new Restaurante(); //instanciar objeto
+        rest1.añadirPulpo(78); //añadir valor pulpo
+        rest1.mostrarPulpo(); //mostrar valor pulpo
+        rest1.añadirPatatas(16); //añadir valor patatas
+        rest1.mostrarPatatas(); //mostrar valor patatas
+        rest1.calcularPersonas(); //utilizar método para calculas el número de personas
+        System.out.println("Cantidad personas = "+rest1.calcularPersonas()); //mostrar resultado
     }
     
 }
