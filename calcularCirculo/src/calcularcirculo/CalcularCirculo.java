fd6de00ff4353402d5912172bb1f3c0bf4646689
Jesus Quesada <jmqa82@gmail.com>
2016-10-19 08:57:31 +0200

    Update CalcularCirculo.java

--- calcularCirculo/src/calcularcirculo/CalcularCirculo.java
+++ calcularCirculo/src/calcularcirculo/CalcularCirculo.java
@@ -4,11 +4,11 @@ package calcularcirculo;
 public class CalcularCirculo {
 
     public static void main(String[] args) {
-        Circulo circulo1 = new Circulo();
-        circulo1.setRadio(5);
-        double res1 = circulo1.calcularArea();
-        double res2 = circulo1.calcularLongitud();
-        System.out.println("Area = "+res1+ "Longitud = "+res2);
+        Circulo circulo1 = new Circulo(); //instanciar objeto
+        circulo1.setRadio(5); //añadir valor radio
+        double res1 = circulo1.calcularArea(); //utilizar método para calcular el area
+        double res2 = circulo1.calcularLongitud(); //utilizar método para calcular la longitud
+        System.out.println("Area = "+res1+ "Longitud = "+res2); //mostrar resultado
     }
     
 }
